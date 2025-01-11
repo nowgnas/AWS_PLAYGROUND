904c7391d1e0929d6a5b724023df18aec0f5d39c
nowgnas <dev.nowgnas@gmail.com>
2025-01-11 23:10:56 +0900

    숫자 맞추기 api 추가

--- src/main/java/com/example/aws/test/DemoController.java
+++ src/main/java/com/example/aws/test/DemoController.java
@@ -1,7 +1,9 @@
 package com.example.aws.test;
 
+import java.time.LocalDate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,16 @@ public class DemoController {
     @GetMapping("test/content")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok().body("hello spring boot");
+    }
+
+    @GetMapping("guess/{number}")
+    public ResponseEntity<String> guess(@PathVariable int number) {
+        LocalDate today = LocalDate.now();
+        int answer = (today.getYear() + today.getMonthValue() * today.getDayOfMonth()) % 100;
+        if (number == answer) {
+            return ResponseEntity.ok().body("정답입니다!!");
+        } else {
+            return ResponseEntity.ok().body("아쉽네요.. 틀렸습니다!");
+        }
     }
 }
