8b0b77c83c90b0cda45bdcd2bec2c7cfe84baa87
nowgnas <dev.nowgnas@gmail.com>
2025-01-11 23:31:25 +0900

    업다운 게임 보완

--- src/main/java/com/example/aws/test/DemoController.java
+++ src/main/java/com/example/aws/test/DemoController.java
@@ -14,14 +14,25 @@ public class DemoController {
         return ResponseEntity.ok().body("hello spring boot");
     }
 
-    @GetMapping("guess/{number}")
-    public ResponseEntity<String> guess(@PathVariable int number) {
-        LocalDate today = LocalDate.now();
-        int answer = (today.getYear() + today.getMonthValue() * today.getDayOfMonth()) % 100;
-        if (number == answer) {
-            return ResponseEntity.ok().body("정답입니다!!");
-        } else {
-            return ResponseEntity.ok().body("아쉽네요.. 틀렸습니다!");
+    @GetMapping("guess/{req}")
+    public ResponseEntity<String> guess(@PathVariable String req) {
+        try {
+            int number = Integer.parseInt(req);
+
+            if (number < 1 || number > 100) {
+                return ResponseEntity.ok().body("1부터 100 사이의 숫자를 입력해 주세요!! ");
+            }
+            LocalDate today = LocalDate.now();
+            int answer = (today.getYear() + today.getMonthValue() * today.getDayOfMonth()) % 100;
+            if (number > answer) {
+                return ResponseEntity.ok().body("아쉽네요.. down!!");
+            } else if (number < answer) {
+                return ResponseEntity.ok().body("아쉽네요.. up!!");
+            } else {
+                return ResponseEntity.ok().body("정답입니다!!");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.ok().body("1부터 100 사이의 숫자를 입력해 주세요!! ");
         }
     }
 }
